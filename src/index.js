f9ab81e4327364b96bb7a657ce805a1b8e55c3c9
KnacKWursTKinG <knackwurstking.tux@gmil.com>
2021-08-06 20:26:41 +0200

    remove Root componenet & modify #root in theme

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
 export { default as Button } from './components/Button/Button.jsx'
 export { default as Input } from './components/Input/Input.jsx'
-export { default as Root } from './components/Container/Root.jsx'
