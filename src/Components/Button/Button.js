b297e63a9ff893ea9710371ed7ff36d814bc1076
KnacKWursTKinG <knackwurstking.tux@gmil.com>
2021-08-12 19:41:16 +0200

    some eslint fixes

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './button.css'
+
+const Button = ({ style, className, shadow, disabled, children, ...props }) => {
+  const classNames = [
+    'king-ui-btn',
+    (disabled) ? 'king-ui-btn--disabled' : '',
+    className || ''
+  ]
+
+  return (
+    <button
+      style={{
+        boxShadow: (shadow) ? (disabled ? 'var(--theme-box-shadow--disabled)' : 'var(--theme-box-shadow)') : 'none',
+        ...style
+      }}
+      className={classNames.join(' ')}
+      disabled={disabled}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
+
+Button.propTypes = {
+  style: PropTypes.object,
+  className: PropTypes.string,
+  shadow: PropTypes.bool,
+  disabled: PropTypes.bool,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.object.node),
+    PropTypes.node.isRequired
+  ])
+}
+
+export default Button
---
+++ src/Components/Nav/MenuOverlay.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const NavMenuOverlay = ({ style, className, zIndex, space, children }) => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        position: 'fixed',
+        zIndex: zIndex,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 'unset',
+        fontFamily: 'unset',
+        background: 'var(--theme-nav-background)',
+        top: 0,
+        left: 0,
+        width: '100vw',
+        height: '100vh',
+        ...style
+      }}
+      className={className}
+    >
+      {(children instanceof Array)
+        ? (children.map((child, idx) => {
+            return (
+              <div key={idx} style={{ padding: space }}>
+                {child}
+              </div>
+            )
+          }))
+        : children}
+    </div>
+  )
+}
+
+NavMenuOverlay.defaultProps = {
+  zIndex: 500,
+  space: '1em'
+}
+
+NavMenuOverlay.propTypes = {
+  style: PropTypes.object,
+  className: PropTypes.string,
+  zIndex: PropTypes.number,
+  space: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node.isRequired),
+    PropTypes.node.isRequired
+  ])
+}
+
+export default NavMenuOverlay
