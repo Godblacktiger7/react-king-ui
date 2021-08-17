a9b2aa58409720cb2e0aae7cbccbf06670aed4e8
KnacKWursTKinG <knackwurstking.tux@gmil.com>
2021-08-17 08:59:39 +0200

    some basic stuff to getting started

---
+++ examples/NavMenuBar/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { Text } from 'king-ui'
+import 'king-ui/Theme/DarkBlue.css'
+
+const App = () => {
+  return (
+    <Text fontSize='2em' underline shadow>Nav Menu Bar Example</Text>
+  )
+}
+
+export default App
